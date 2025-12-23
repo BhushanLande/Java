710cc4ead04f78e84643104f3c69c92f09b72a11
BhushanLande <b.lande525@gmail.com>
2025-12-23 20:14:18 +0530

    Updated Collection Programs

--- src/main/java/logicalProgram/CollectionPrograms.java
+++ src/main/java/logicalProgram/CollectionPrograms.java
@@ -17,10 +17,28 @@ public class CollectionPrograms {
 
         LinkedHashSet linkedHashSet = new LinkedHashSet(Arrays.asList(array));
         System.out.println(linkedHashSet);
+
         findDuplicates(arrray);
 
         reverseTheArray(arrray);
 
+        findSmallestAndLargestNum(arrray);
+
+        useIterator(arrray);
+
+        removeDuplicates(arrray);
+
+        List<Integer> list1 = Arrays.asList(1, 2, 3, 4);
+        List<Integer> list2 = Arrays.asList(3, 4, 5, 6);
+
+        findCommonsInList(list1, list2);
+
+        findUniquesInList(list1, list2);
+    }
+
+    public static void removeDuplicates(ArrayList list){
+        TreeSet<Integer> values = new TreeSet<>(list);
+        System.out.println(values);
     }
     public static void  findDuplicates(ArrayList list){
         HashSet unique = new HashSet<>();
@@ -36,4 +54,27 @@ public class CollectionPrograms {
         Collections.reverse(list);
         System.out.println("Reversed collection: "+list);
     }
+    public static void findSmallestAndLargestNum(ArrayList list){
+        System.out.println("Smallest num from list is: "+Collections.min(list));
+        System.out.println("Largest num from list is: "+Collections.max(list));
+    }
+    public static void useIterator(ArrayList list){
+        Iterator it = list.iterator();
+        while (it.hasNext()){
+            System.out.println(it.next());
+        }
+    }
+    public static void findCommonsInList(List list1, List list2){
+        Set<Integer> set = new HashSet<>(list2);
+        set.retainAll(list1);
+        System.out.println("Common values are: "+set);
+    }
+
+    public static void findUniquesInList(List list1, List list2){
+        Set<Integer> set = new HashSet<>(list2);
+        set.removeAll(list1);
+        System.out.println("Unique values are: "+set);
+    }
+
+
 }
